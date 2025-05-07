b5d19a24f78c4eda2e488935f5544c9a01f754c6
KAUAN_DEV⌨️ <contatemporaria6565@gmail.com>
2025-05-07 16:38:14 -0300

    005 henrique

--- javascript/index.js
+++
@@ -1,7 +0,0 @@
-
-const menuBtn = document.getElementById('menuBtn');
-const navMenu = document.getElementById('navMenu');
-
-menuBtn.addEventListener('click', () => {
-  navMenu.style.display = navMenu.style.display === 'block' ? 'none' : 'block';
-});
